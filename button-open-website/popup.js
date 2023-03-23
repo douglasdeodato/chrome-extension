b4d4dc006571a4b3a1cd9055438f4c48ded780d4
Douglas Lima <douglasdeodato@gmail.com>
2023-03-23 15:23:26 +0000

    testing

--- button-open-website/popup.js
+++ button-open-website/popup.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((response) => response.json())
       .then((data) => {
         var url = data.exampleUrl || "https://www.example.com";
-        chrome.tabs.update({ url: url });
+        chrome.tabs.create({ url: url }, function(tab) {
+          if (chrome.runtime.lastError) {
+            console.error("Error occurred:", chrome.runtime.lastError);
+          } else {
+            console.log("Tab created with URL:", url);
+            chrome.tabs.executeScript(tab.id, { code: 'document.getElementById("button-test").click();' });
+          }
+        });
       })
       .catch((error) => {
         console.error("Error occurred:", error);
